a9163779dfc8f5def99121ca9185d4cbfc4f996f
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2023-09-09 00:28:02 +0200

    final

--- scripts.js
+++ scripts.js
@@ -1,62 +1,64 @@
-matches = books
-page = 1;
-range = [1,2];
+// matches = books
+// page = 1;
+// range = [1,2];
 
-if (!books && !Array.isArray(books)) throw new Error('Source required') 
-if (!range && range.length < 2) throw new Error('Range must be an array with two numbers')
+// if (!books && !Array.isArray(books)) throw new Error('Source required') 
+// if (!range && range.length < 2) throw new Error('Range must be an array with two numbers')
 
-function createPreview(author, id, image, title) {
-    console.log(author, id, image, title)
+// function createPreview(author, id, image, title) {
+//     console.log(author, id, image, title)
 
-}
+// }
 
-day = {
-    dark: '10, 10, 20',
-    light: '255, 255, 255',
-}
+// day = {
+//     dark: '10, 10, 20',
+//     light: '255, 255, 255',
+// }
 
-night = {
-    dark: '255, 255, 255',
-    light: '10, 10, 20',
-}
+// night = {
+//     dark: '255, 255, 255',
+//     light: '10, 10, 20',
+// }
 
-fragment = document.createDocumentFragment()
-const extracted = books.slice(0, 36)
-console.log(extracted)
+// fragment = document.createDocumentFragment()
+// const extracted = books.slice(0, 36)
+// console.log(extracted)
 
-for (i=0; extracted; i++) {
-    book = extracted[i]
-    const preview = createPreview(book.author, book.id, book.image, book.title)
+// for (i=0; extracted; i++) {
+//     book = extracted[i]
+//     const preview = createPreview(book.author, book.id, book.image, book.title)
 
-    fragment.appendChild(preview)
-}
+//     fragment.appendChild(preview)
+// }
 
-data-list-items.appendChild(fragment)
+// data-list-items.appendChild(fragment)
 
-genres = document.createDocumentFragment()
-element = document.createElement('option')
-element.value = 'any'
-element = 'All Genres'
-genres.appendChild(element)
+// genres = document.createDocumentFragment()
+// element = document.createElement('option')
+// element.value = 'any'
+// element = 'All Genres'
+// genres.appendChild(element)
 
-for ([id, name]; Object.entries(genres); i++) {
-    document.createElement('option')
-    element.value = value
-    element.innerText = text
-    genres.appendChild(element)
-}
+// for ([id, name]; Object.entries(genres); i++) {
+//     document.createElement('option')
+//     element.value = value
+//     element.innerText = text
+//     genres.appendChild(element)
+// }
 
-data-search-genres.appendChild(genres)
+// data-search-genres.appendChild(genres)
 
-authors = document.createDocumentFragment()
-element = document.createElement('option')
-element.value = 'any'
-element.innerText = 'All Authors'
-authors.appendChild(element)
+// authors = document.createDocumentFragment()
+// element = document.createElement('option')
+// element.value = 'any'
+// element.innerText = 'All Authors'
+// authors.appendChild(element)
 
-for ([id, name];Object.entries(authors); id++) {
-    document.createElement('option')
-    element.value = value
-    element = text
-    authors.appendChild(element)
-}
+// for ([id, name];Object.entries(authors); id++) {
+//     document.createElement('option')
+//     element.value = value
+//     element = text
+//     authors.appendChild(element)
+// }
+
+
